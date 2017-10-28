77d285b8ea3d07283e9535f3c6b6fa6aeb9fa1c7
jloures <john_loures@hotmail.com>
2017-10-28 01:35:13 -0400

    More core functions added

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
-function generateAll(numbersInGame, numbersPerRow, numbersToGuarantee) {
-    //create array with all possible numbers
-    let numbersNotPicked = [];
-    for(let i = 1; i <= numbersInGame; i++) {
-        numbersNotPicked.push(i);
+function generateAll(arrayWithNumbers, numbersPerRow, numbersToGuarantee) {
+    let sizeOfPickNotVarying = numbersToGuarantee - 1;
+    let sizeOfPickVarying = numbersPerRow - sizeOfPickNotVarying;
+    let combinationsArray = [];
+    let combinationNumber = 1;
+    for(let i = 0; i < arrayWithNumbers.length - numbersPerRow; i += 1) {
+        let combinationArray = [];
+        for(let j = i; (j - i) < sizeOfPickNotVarying; j++) {
+            combinationArray[j-i] = arrayWithNumbers[j];
+        }
+        for(let z = (i + sizeOfPickNotVarying); z < arrayWithNumbers.length; z += sizeOfPickVarying ) {
+            for(let k = 0; (k + sizeOfPickNotVarying)< numbersPerRow; k++ ) {
+                let newNumber = arrayWithNumbers[k + z];
+                if( newNumber ) {
+                        combinationArray[k + sizeOfPickNotVarying] = newNumber;
+                }
+            }
+            combinationsArray.push(combinationArray);
+            combinationNumber++;
+        }
     }
-
-
-
+    console.log(combinationNumber);
+    return combinationsArray;
 }
 
-function generateCombinations (startNumber, endNumber, totalNumbersToBePicked, generateCombinationsFunctionCallback = () => {}) {
-    let generateCombinations =  (currentIndex, startNumber, endNumber, totalNumbersToBePicked, arrayOfNumbersPicked) => {
+function generateCombinations (startNumber, endNumber, totalNumbersToBePicked, arrayOfNumbers) {
+    var totalCombinationsGenerated = 0;
+    var arrayWithCombinations = [];
+    let generateCombinations =  (startNumber, endNumber, numbersLeft, arrayOfNumbersPicked) => {
         
-        if( totalNumbersToBePicked === 0 ) {
+        if( numbersLeft === 0 ) {
             return;
         }
         for(let i = startNumber; i <= endNumber; i++) {
-            arrayOfNumbersPicked[currentIndex - totalNumbersToBePicked] = i;
-            generateCombinations(currentIndex, i + 1, endNumber, totalNumbersToBePicked - 1, arrayOfNumbersPicked);
-            if(totalNumbersToBePicked == 1) {
-                generateCombinationsFunctionCallback();
+            arrayOfNumbersPicked[totalNumbersToBePicked - numbersLeft] = arrayOfNumbers[i-1];
+            generateCombinations(i + 1, endNumber, numbersLeft - 1, arrayOfNumbersPicked);
+            if(numbersLeft === 1) {
+                //add anything custom you want here
+                totalCombinationsGenerated++;
+                arrayWithCombinations.push(cloneArray(arrayOfNumbersPicked));
             }
         }
     }
-    generateCombinations(totalNumbersToBePicked, startNumber, endNumber, totalNumbersToBePicked, []);
+    generateCombinations(startNumber, endNumber, totalNumbersToBePicked, []);
+    return arrayWithCombinations;
 }
 
 function pickRandomNumbers(arrayOfNumbersToPick, numbersToPick) {
@@ -43,12 +62,61 @@ function pickRandomNumbers(arrayOfNumbersToPick, numbersToPick) {
     return arrayOfNumbersPicked;
 }
 
-function generateArray(startNumber, endNumber) {
+function cloneArray(originalArray) {
+    let cloneArray = [];
+    for(let i = 0; i < originalArray.length; i++) {
+        cloneArray.push(originalArray[i]);
+    }
+    return cloneArray;
+}
+
+function generateArray(startNumber, endNumber, isRandom) {
     let numbersArray = [];
     for(let i = startNumber; i <= endNumber; i++) {
         numbersArray.push(i);
     }
+    if( isRandom ) {
+        for (let i = numbersArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [numbersArray[i], numbersArray[j]] = [numbersArray[j], numbersArray[i]];
+        }
+    }
     return numbersArray;
 }
 
-generateCombinations (1, 49, 3);
+function generateGuarantee(totalNumbers, numbersPerRow, numbersToGuarantee) {
+    let randomNumberArray = generateArray(1,totalNumbers,true);
+    console.log(randomNumberArray)
+    let combinationsArray = generateAll(randomNumberArray, numbersPerRow, numbersToGuarantee);
+    return combinationsArray;
+}
+
+function test() {
+
+    let combinationsArray = generateGuarantee(49, 6, 3);
+    let newArray = generateArray(1,49,false);
+    let arrayOfCombinationsOf3 = generateCombinations(1, 49, 3, newArray);
+    for(let j = 0; j < arrayOfCombinationsOf3.length; j++) {
+        let current3Combination = arrayOfCombinationsOf3[j];
+        let o = 1;
+        console.log(combinationsArray.length)
+        console.log("here")
+        console.log(current3Combination)
+        if( !combinationsArray.some( 
+            combination => {
+                for(let z = 0; z < 3; z++) {
+                    if( !combination.some( num => num === current3Combination[z])) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        )) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(test());
+
